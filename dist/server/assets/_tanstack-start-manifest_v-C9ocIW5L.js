4a29f8af6a076f1f009bb5d515a5929cf4c8fe9e
David @ home <ftc.13901@gmail.com>
2026-08-07 16:04:46 -0500

    refactor: update start manifest and router files, replace old manifest with new version
    - Added new start manifest file `_tanstack-start-manifest_v-C9ocIW5L.js` with updated routes and assets.
    - Removed old start manifest file `_tanstack-start-manifest_v-DKbeEB5N.js`.
    - Introduced new router file `router-vGWGfr6I.js` with updated route definitions and components.
    - Updated server to import the new start manifest and router files.
    - Changed build command in `netlify.toml` to use `npm run build`.
    - Updated build script in `package.json` to directly call Vite's build command.

---
+++ dist/server/assets/_tanstack-start-manifest_v-C9ocIW5L.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/David/SchoolDashboard/src/routes/__root.tsx", children: ["/", "/customize"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-DYmUK-uc.css", type: "text/css" } }], preloads: ["/assets/index-DLdz30hA.js"] }, "/": { filePath: "C:/Users/David/SchoolDashboard/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-BLCbYCXs.js", "/assets/Header-D9musd1G.js"] }, "/customize": { filePath: "C:/Users/David/SchoolDashboard/src/routes/customize.tsx", children: void 0, assets: [], preloads: ["/assets/customize-rcNiqTy6.js", "/assets/Header-D9musd1G.js"] } }, clientEntry: "/assets/index-DLdz30hA.js" });
+export {
+  tsrStartManifest
+};
--- dist/server/assets/_tanstack-start-manifest_v-DKbeEB5N.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/David/SchoolDashboard/src/routes/__root.tsx", children: ["/", "/customize"], assets: [{ tag: "link", attrs: { rel: "stylesheet", href: "/assets/index-Bwgie4Gr.css", type: "text/css" } }], preloads: ["/assets/index-DMu5yRWJ.js"] }, "/": { filePath: "C:/Users/David/SchoolDashboard/src/routes/index.tsx", children: void 0, assets: [], preloads: ["/assets/index-D_EZU15w.js", "/assets/Header-Df5KmkyS.js"] }, "/customize": { filePath: "C:/Users/David/SchoolDashboard/src/routes/customize.tsx", children: void 0, assets: [], preloads: ["/assets/customize-BQGNnYRq.js", "/assets/Header-Df5KmkyS.js"] } }, clientEntry: "/assets/index-DMu5yRWJ.js" });
-export {
-  tsrStartManifest
-};
